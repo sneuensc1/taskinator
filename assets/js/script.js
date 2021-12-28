1150694ed5528f8060b7599b0589871e3f294443
saraneuie <sneuensc@umich.edu>
2021-12-28 15:24:28 -0500

    finish form

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,14 @@ var taskFormHandler = function (event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;  
 
+    //check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset ();
+    
     //package data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -23,15 +31,14 @@ var createTaskEl = function(taskDataObj) {
 
 //create div to hold task info and add to list item
 var taskInfoEl = document.createElement("div");
-//give it a class name
 taskInfoEl.className = "task-info";
 //add html content to div
-taskInfoEl.innerHTML = "<h3 class= 'task-name'>" + taskDataObj.name + "</h3><span class= 'task-type>" + taskDataObj.type + "</span>";
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
 listItemEl.appendChild(taskInfoEl);
 
 //add entire list item to list
 tasksToDoEl.appendChild(listItemEl);
-}
+};
 
 formEl.addEventListener("submit", taskFormHandler);
